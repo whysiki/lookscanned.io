647698f2b8938fd4698f3978c86f1c0f3bade343
whysiki <whysiki@proton.me>
2025-01-11 09:23:00 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lookscanned.io/',
   assetsInclude: ["**/*.pfb", "**/*.ttf"],
   plugins: [
     vue(),
